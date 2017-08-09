6cd1d5c2408106e510fc29167f4ee9107e24f866
Tait Larson <tait@stylemepretty.com>
2017-08-09 15:23:08 -0400

    Updated README and got one test working.

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { MemoryRouter as Router} from 'react-router-dom' // 4.0.0
 import App from './App'
-
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
- is not required.
-**/
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<Router><App /></Router>, div)
 })
+
 
 
